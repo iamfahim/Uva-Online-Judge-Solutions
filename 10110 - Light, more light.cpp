4691f1b717133fa8ecc9dd819c3e8410665a5a86
iamfahim <mislamfahim@gmail.com>
2019-10-14 02:43:39 +0600

    check n is srqt? and use unsigned int

---
+++ 10110 - Light, more light.cpp
@@ -0,0 +1,26 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define PI             acos(-1.0)
+#define IOS            ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define inf            1LL<<62  // this is minus inf
+#define ms(a,b)         memset(a, b, sizeof(a))
+#define check          cout<<"I am in here BRO!"<<endl;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+const int MOD = (int)1e9+7;
+int  main()
+{
+    IOS
+    unsigned int t,n;
+    while(cin>>n)
+    {
+        
+        unsigned int nn = sqrt(n);
+        if(n==0)break;
+        else if((nn*nn)==n)printf("yes\n");
+        else printf("no\n");
+    }
+    return 0;
+}
