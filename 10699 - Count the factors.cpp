1a67d78589ca47391bf6db66e8916fe78418f343
iamfahim <mislamfahim@gmail.com>
2019-10-27 00:52:56 +0600

    number of differnt prime factors

---
+++ 10699 - Count the factors.cpp
@@ -0,0 +1,46 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define PI             acos(-1.0)
+#define IOS            ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define inf            1LL<<62  // this is minus inf
+#define ms(a,b)         memset(a, b, sizeof(a))
+#define check          cout<<"I am in here BRO!"<<endl;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef map<int, int> mii;
+const int MOD = (int)1e9+7;
+ll _sieve_size;
+bitset<10000010> bs;
+vi primes;
+void sieve(ll upperbound) {
+  _sieve_size = upperbound + 1;
+  bs.set();
+  bs[0] = bs[1] = 0;
+  for (ll i = 2; i <= _sieve_size; i++) if (bs[i]) {
+
+    for (ll j = i * i; j <= _sieve_size; j += i) bs[j] = 0;
+    primes.push_back((int)i);
+} }
+ll numDiffPF(ll N) {
+  ll PF_idx = 0, PF = primes[PF_idx], ans = 0;
+  while (N != 1 && (PF * PF <= N)) {
+    if (N % PF == 0) ans++;
+    while (N % PF == 0) N /= PF;
+    PF = primes[++PF_idx];
+  }
+  if (N != 1) ans++;
+  return ans;
+}
+int  main()
+{
+    IOS
+     sieve(10000000);
+     ll n;
+     while(cin>>n)
+     {
+         if(n==0)break;
+         cout<<n<<" : "<<numDiffPF(n)<<endl;
+     }
+    return 0;
+}
